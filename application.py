2ccebff6d6091e4d1a89d0e93452770dbcd252ab
HectorMiranda <hector.miranda8@upr.edu>
2023-02-20 21:32:30 -0400

    Added example API

--- application.py
+++ application.py
@@ -3,9 +3,21 @@ from flask.json import jsonify
 
 application = Flask(__name__)
 
+@application.route('/', methods=['GET'])
+def home():
+    api = {
+        "/" : "Current view",
+        "/hello" : "Example view of hello world",
+        "/API" : {
+            "/users" : ""
+        }
+    }
+    res = make_response(api)
+    return res
+
 @application.route('/hello', methods=['GET'])
 def hello_world():
-    res = make_response("Hello, World!")
+    res = make_response(jsonify({"res" : "Hello World"}))
     return res
 
 if __name__ == '__main__':
