61c003c1cd6c5c5c2fca13b2dafa99b44fe21b55
HectorMiranda <hector.miranda8@upr.edu>
2023-02-20 20:56:16 -0400

    Debugging

--- application.py
+++ application.py
@@ -1,11 +1,12 @@
-from flask import Flask, jsonify
+from flask import Flask, request, make_response
+from flask.json import jsonify
 
 application = Flask(__name__)
 
 @application.route('/hello', methods=['GET'])
 def hello_world():
-    res = "Hello, World!"
-    return jsonify(res)
+    res = make_response("Hello, World!")
+    return res
 
 if __name__ == '__main__':
     application.run()
