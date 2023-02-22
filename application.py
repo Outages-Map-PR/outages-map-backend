1a9cc5215e754a24f1d95fac58cf17edb3993ab9
HectorMiranda <hector.miranda8@upr.edu>
2023-02-22 11:39:22 -0400

    Flask cors addition
    added flask cors to allow cross origin requests

--- application.py
+++ application.py
@@ -1,12 +1,16 @@
 # APPLICATION LIBRARIES
-from flask import Flask, request
+from flask import Flask, request, make_response
 from flask.json import jsonify
+from flask_cors import CORS, cross_origin
 
 # CONTROLLERS
 from controllers.user import User
 
 application = Flask(__name__)
 application.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+CORS(application, resources={
+    r"/*": {"origins": "*"}
+}, supports_credentials=True)
 
 @application.route('/', methods=['GET'])
 def home():
@@ -29,10 +33,13 @@ def hello_world():
 @application.route('/API/user', methods=['GET'])
 def user_route():
     if request.method == 'GET':
-        res = User().getAllUsers()
+        res = make_response(User().getAllUsers())
+        res.headers['Access-Control-Allow-Origin'] = '*'
         return res
     else:
-        return {"error": "METHOD NOT SUPPORTED IN ROUTE"}
+        res = make_response({"error": "METHOD NOT SUPPORTED IN ROUTE"})
+        res.headers['Access-Control-Allow-Origin'] = '*'
+        return res
 
 if __name__ == '__main__':
     application.run()
