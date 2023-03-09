3f6874b30d433352107430d5fdce4486e51ad23f
malejandramunoz <maria.munoz10@upr.edu>
2023-03-09 15:07:09 -0400

    Added more locust tests

--- stress_tests/locustfile.py
+++ stress_tests/locustfile.py
@@ -3,8 +3,16 @@ from locust import HttpUser, TaskSet, task, between
 
 class UserBehavior(HttpUser):
     @task
+    def all_users(self):
+        self.client.get("/API/user")
+
+    @task
     def login(self):
         self.client.get("/API/user/login", json={
             "user_email": "test2@gmail.com",
             "user_password": "this_is_a_test"
         })
+
+    @task
+    def outage_map(self):
+        self.client.get("/API/outagesmap")
