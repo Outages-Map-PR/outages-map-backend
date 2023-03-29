74bef468bdd70a9598516060705e80c12892f5ac
HectorMiranda <hector.miranda8@upr.edu>
2023-03-29 14:37:58 -0400

    Heroku setup
    Added Procfile and runtime files for Heroku, updated dependencies on requirements

--- config/dbconfig.py
+++ config/dbconfig.py
@@ -1,8 +1,8 @@
 # Local configuration for Outages DataBase
 pg_config = {
-    'dbname': 'outages_db',
-    'user': 'outages_aws',
-    'password': 'jsB3YgMChbeKX158GeXk',
+    'dbname': 'd9b9b39si7p5du',
+    'user': 'jheqavekjqqbjs',
+    'password': 'a72483234af7a10f62d4cf02b14da4baaf015676d2f8eb68f0b3d0d3e0d751b3',
     'port': '5432',
-    'host': 'outages-db.ccm1zgkw4kmr.us-east-1.rds.amazonaws.com'
+    'host': 'ec2-34-193-110-25.compute-1.amazonaws.com'
 }
